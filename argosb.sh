1fe6bf24d7b89f381168d1bb932b5591dc8b2a35
yanfeng110 <yanfeng_feng@msn.cn>
2025-08-27 16:10:33 +0800

    Update argosb.sh

--- argosb.sh
+++ argosb.sh
@@ -44,13 +44,7 @@ echo "双栈VPS显示IPv6节点配置命令：ippz=6 agsb list 【或者】 ippz
 echo "---------------------------------------------------------"
 echo
 }
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-echo "甬哥Github项目 ：github.com/yonggekkk"
-echo "甬哥Blogger博客 ：ygkkk.blogspot.com"
-echo "甬哥YouTube频道 ：www.youtube.com/@ygkkk"
-echo "ArgoSB一键无交互小钢炮脚本💣"
-echo "当前版本：V25.8.18"
-echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
 hostname=$(uname -a | awk '{print $2}')
 op=$(cat /etc/redhat-release 2>/dev/null || cat /etc/os-release 2>/dev/null | grep -i pretty_name | cut -d \" -f2)
 [ -z "$(systemd-detect-virt 2>/dev/null)" ] && vi=$(virt-what 2>/dev/null) || vi=$(systemd-detect-virt 2>/dev/null)
